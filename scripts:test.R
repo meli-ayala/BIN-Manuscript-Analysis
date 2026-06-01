0389d572dff5b0bf83b6a0f187cfde702c144560
Mels Ayala <melsayala@Melss-MacBook-Air-10.local>
2026-06-01 14:21:07 -0400

    remove old script

--- scripts:test.R
+++
@@ -1,8 +0,0 @@
-#testing for commit change
-#test 2
-#mra test to mb
-#mra test to github
-#mra test to github after going crazy
-#mra editing the script in Github
-#mb test 3
-#mra test github again
